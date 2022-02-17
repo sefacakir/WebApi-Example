087a8f67f0856c7874cddc924979171799b0fd2c
sefacakir <cakirsefa72@gmail.com>
2022-02-17 23:09:27 +0300

    Add project files.

---
+++ HotelFinder.API/Controllers/HotelsController.cs
@@ -0,0 +1,55 @@
+﻿using HotelFinder.Business.Abstract;
+using HotelFinder.Business.Concrete;
+using HotelFinder.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelFinder.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelsController : ControllerBase
+    {
+        private IHotelService _hotelService;
+        public HotelsController(IHotelService hotelService)
+        {
+            _hotelService = hotelService;
+        }
+
+        [HttpGet]
+        public List<Hotel> GetAll()
+        {
+            return _hotelService.GetAllHotels();
+        }
+
+        [HttpGet("{id}")]
+        public Hotel Get(int id)
+        {
+            var hotel = _hotelService.GetHotelById(id);
+            return hotel;
+        }
+
+        [HttpPost]
+        public Hotel Add([FromBody] Hotel hotel)
+        {
+            _hotelService.CreateHotel(hotel);
+            return hotel;
+        }
+
+        [HttpPut]
+        public Hotel Update([FromBody] Hotel hotel)
+        {
+            _hotelService.UpdateHotel(hotel);
+            return hotel;
+        }
+
+        [HttpDelete("{Id}")]
+        public void Delete(int id)
+        {
+            _hotelService.DeleteHotel(id);
+        }
+    }
+}
